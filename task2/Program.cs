0ed19cee35f0c59a204daf6f92a0688e4e4e28dc
RomanKosov <kosovr48@gmail.com>
2023-12-01 23:10:46 +0500

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте массив заполненный случайными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(100, 1000);
+}
+
+void ReleaseArray(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+            count ++;
+    }
+    Console.WriteLine(count);
+}
+
+Console.Clear();
+Console.WriteLine("Введите число элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine(string.Join(", ", array));
+ReleaseArray(array);
+
