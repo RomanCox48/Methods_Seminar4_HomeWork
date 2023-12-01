dc61d6910636fc45c27b763983a8127bccdb15d0
RomanKosov <kosovr48@gmail.com>
2023-12-01 23:09:38 +0500

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,41 @@
+﻿// Напишите программу, которая бесконечно запрашивает целые числа с консоли. 
+// Программа завершается при вводе символа ‘q’ или при вводе числа, 
+// сумма цифр которого четная. 
+
+Console.WriteLine("Введите целые числа. Для завершения введите 'q'.");
+while (true)
+{
+    string input = Console.ReadLine();
+    if (input.ToLower() == "q")
+    {
+        Console.WriteLine("Программа завершена");
+        break;
+    }
+    if (int.TryParse(input, out int number))
+    {
+        if (IsEvenDigitSum(number))
+        {
+            Console.WriteLine($"Сумма цифр числа {input} четная. Программа завершена.");
+            break;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Некорректный ввод. Введите целое число или 'q' для завершения.");
+    }
+}
+
+bool IsEvenDigitSum(int number)
+{
+    int digitSum = 0;
+
+    // Вычисляем сумму цифр числа
+    while (number != 0)
+    {
+        digitSum += number % 10;
+        number /= 10;
+    }
+
+    // Проверяем, является ли сумма цифр четной
+    return digitSum % 2 == 0;
+}
